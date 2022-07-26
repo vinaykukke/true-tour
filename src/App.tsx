1ccce3f2820f96974e4280c5c23bd84891b66f60
vinay kukke <kukke06@gmail.com>
2022-07-26 12:09:46 +0530

    Dynamic sizing for threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import * as THREE from "three";
 import setup from "./three-js/setup";
 import Pano from "./components/Pano";
@@ -17,94 +17,111 @@ const clickMouse = new THREE.Vector2();
 const moveMouse = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
 
-const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-  const doubleClick = event.detail === 2;
-  const singleClick = event.detail === 1;
+function App() {
+  const rootRef = useRef(null);
 
-  /** Drop the object */
-  if (singleClick && draggableObject) {
-    draggableObject = null;
-    return;
-  }
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const doubleClick = event.detail === 2;
+    const singleClick = event.detail === 1;
 
-  /** calculate pointer position in normalized device coordinates (-1 to +1) */
-  clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    /** Drop the object */
+    if (singleClick && draggableObject) {
+      draggableObject = null;
+      return;
+    }
 
-  /** Set the camera from which the ray should orginate and to what coordinates it should go to */
-  raycaster.setFromCamera(clickMouse, camera);
-
-  /**
-   * Calculate objects intersecting the picking ray.
-   * The array is sorted. Meaning that closest intersecting object is at position 0.
-   */
-  const found = raycaster.intersectObjects<
-    THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>
-  >(scene.children);
-  const obj = found.length > 0 && found[0].object;
-
-  if (obj.userData.draggable) {
-    if (doubleClick) draggableObject = obj;
-    selectedObject = obj;
-  }
-
-  /** Clear selectedObject once focus is shifted from hotspot */
-  if (obj.name === "mesh__pano") selectedObject = null;
-
-  /** Removes the focus of newly added hotspot */
-  const hs = document.getElementsByClassName("hotspot_new__focus");
-  for (let i = 0; i < hs.length; i++) {
-    const element = hs[i];
-    element.classList.remove("hotspot_new__focus");
-    selectedObject = null;
-  }
-};
-
-const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-  /** calculate pointer position in normalized device coordinates (-1 to +1) */
-  moveMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  moveMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-};
-
-const handleZoom = (event: React.WheelEvent<HTMLDivElement>) => {
-  const zoom = camera.zoom;
-  const zoomIn = event.deltaY < 0 && zoom <= DEFAULT_DATA.camera_zoom__max;
-  const zoomOut = event.deltaY > 0 && zoom >= DEFAULT_DATA.camera_zoom__min;
-
-  if (zoomIn) camera.zoom = zoom + 0.2;
-  if (zoomOut) camera.zoom = zoom - 0.2;
-
-  /** Update the projection matrix */
-  camera.updateProjectionMatrix();
-};
-
-const dragObject = () => {
-  if (draggableObject) {
-    /** Disable OrbitalControl if user is dragging object */
-    controls.disable();
+    /** calculate pointer position in normalized device coordinates (-1 to +1) */
+    clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
     /** Set the camera from which the ray should orginate and to what coordinates it should go to */
-    raycaster.setFromCamera(moveMouse, camera);
+    raycaster.setFromCamera(clickMouse, camera);
 
+    /**
+     * Calculate objects intersecting the picking ray.
+     * The array is sorted. Meaning that closest intersecting object is at position 0.
+     */
     const found = raycaster.intersectObjects<
       THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>
     >(scene.children);
+    const obj = found.length > 0 && found[0].object;
 
-    if (found.length > 0) {
-      found.forEach((item) => {
-        /** If the intersected object is not a pano OR is a hotspot the skip it and move to next item */
-        if (item.object.userData.type !== "pano") return;
-
-        draggableObject.position.copy(item.point.clone());
-      });
+    if (obj.userData.draggable) {
+      if (doubleClick) draggableObject = obj;
+      selectedObject = obj;
     }
 
-    /** Enable OrbitalControl once the dragging is finished */
-    controls.enable();
-  }
-};
+    /** Clear selectedObject once focus is shifted from hotspot */
+    if (obj.name === "mesh__pano") selectedObject = null;
 
-function App() {
+    /** Removes the focus of newly added hotspot */
+    const hs = document.getElementsByClassName("hotspot_new__focus");
+    for (let i = 0; i < hs.length; i++) {
+      const element = hs[i];
+      element.classList.remove("hotspot_new__focus");
+      selectedObject = null;
+    }
+  };
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    /** calculate pointer position in normalized device coordinates (-1 to +1) */
+    moveMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    moveMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  };
+
+  const handleZoom = (event: React.WheelEvent<HTMLDivElement>) => {
+    const zoom = camera.zoom;
+    const zoomIn = event.deltaY < 0 && zoom <= DEFAULT_DATA.camera_zoom__max;
+    const zoomOut = event.deltaY > 0 && zoom >= DEFAULT_DATA.camera_zoom__min;
+
+    if (zoomIn) camera.zoom = zoom + 0.2;
+    if (zoomOut) camera.zoom = zoom - 0.2;
+
+    /** Update the projection matrix */
+    camera.updateProjectionMatrix();
+  };
+
+  const resizeRendererToDisplaySize = (root: HTMLElement) => {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize =
+      width !== root.clientWidth || height !== root.clientHeight;
+
+    if (needResize) {
+      renderer.setSize(root.clientWidth, root.clientHeight);
+      labelRenderer.setSize(root.clientWidth, root.clientHeight);
+    }
+
+    return needResize;
+  };
+
+  const dragObject = () => {
+    if (draggableObject) {
+      /** Disable OrbitalControl if user is dragging object */
+      controls.disable();
+
+      /** Set the camera from which the ray should orginate and to what coordinates it should go to */
+      raycaster.setFromCamera(moveMouse, camera);
+
+      const found = raycaster.intersectObjects<
+        THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>
+      >(scene.children);
+
+      if (found.length > 0) {
+        found.forEach((item) => {
+          /** If the intersected object is not a pano OR is a hotspot the skip it and move to next item */
+          if (item.object.userData.type !== "pano") return;
+
+          draggableObject.position.copy(item.point.clone());
+        });
+      }
+
+      /** Enable OrbitalControl once the dragging is finished */
+      controls.enable();
+    }
+  };
+
   useEffect(() => {
     /** Initial Setup */
     setup();
@@ -116,16 +133,16 @@ function App() {
     /** Set the position of the pano */
     pano.position.set(0, 0, 0);
 
-    const hotspot = Hotspot();
-    hotspot.position.set(60, 0, -195);
-
-    /** Making a group */
-    pano.add(hotspot);
-
     /** Add to scene */
     scene.add(pano); // World Space
 
     function animate() {
+      if (resizeRendererToDisplaySize(rootRef.current)) {
+        camera.aspect =
+          rootRef.current.clientWidth / rootRef.current.clientHeight;
+        camera.updateProjectionMatrix();
+      }
+
       /** Only required if controls.enableDamping = true, or if controls.autoRotate = true */
       controls.update();
 
@@ -172,6 +189,7 @@ function App() {
   return (
     <Suspense fallback={null}>
       <div
+        ref={rootRef}
         id="three-js__root"
         onClick={handleClick}
         onMouseMove={handleMouseMove}
