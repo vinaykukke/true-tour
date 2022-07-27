a006af250399bf5d4e74df6af7edde9ff4ef273c
vinay kukke <kukke06@gmail.com>
2022-07-27 12:15:42 +0530

    Theming the add and delete buttons

--- src/components/Tools/index.tsx
+++ src/components/Tools/index.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import IconButton from "@mui/material/IconButton";
 import Hotspot from "src/components/Hotspot";
 import { removeHotspot } from "src/helpers/dispose";
 import { THotspotType } from "src/types/hotspot";
@@ -32,24 +35,32 @@ const Tools = (props) => {
     const { selectedObj } = threeCxt;
     /** Remove the selected object from the scene */
     if (selectedObj) {
-      selectedObj.parent?.remove(selectedObj);
+      selectedObj.parent.remove(selectedObj);
       removeHotspot(selectedObj);
       setSelectedObj(null);
     }
   };
 
   return (
-    <>
-      <div className="hotspot__add" onClick={addHotspot("right")}>
-        Add Hotspot
-      </div>
-      <div className="hotspot__delete" onClick={deleteHotspot}>
-        Delete Hotspot
-      </div>
+    <div className="toolbar">
+      <IconButton
+        className="hotspot__add"
+        aria-label="add"
+        onClick={addHotspot("right")}
+      >
+        <AddIcon />
+      </IconButton>
+      <IconButton
+        className="hotspot__delete"
+        aria-label="add"
+        onClick={deleteHotspot}
+      >
+        <DeleteForeverRoundedIcon />
+      </IconButton>
       {hotspots.map((hs, i) => (
         <Hs onMouseMove={props.onMouseMove} mesh={hs} key={i} tabIndex={i} />
       ))}
-    </>
+    </div>
   );
 };
 
